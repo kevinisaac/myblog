f1f7c80414f17aa2dfa39f2dc27f29d76a718a17
Kevin Isaac <kevin.isaac70@gmail.com>
2014-08-12 01:33:07 +0530

    Initial commit
    Complete re-attempt by NodeJS

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+// Settings
+app.set('view engine', 'jade');
+app.set('view options', {layout: true});
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/static'));
+app.locals.pretty = true;
+
+// Routes
+app.get('/', function(request, response) {
+    response.render('home');
+});
+
+app.get('/post', function(request, response) {
+    response.render('post');
+});
+
+app.listen(7777);
